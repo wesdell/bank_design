ca19bf1e2808a24e12a576aa6f1abfc3dcab803e
wesdell <wesdell17@gmail.com>
2024-06-06 15:42:27 -0500

    Set bank clients

---
+++ src/LegalClient.java
@@ -0,0 +1,5 @@
+public class LegalClient extends Client {
+  public LegalClient(String name) {
+    super(name);
+  }
+}
---
+++ src/NaturalClient.java
@@ -0,0 +1,5 @@
+public class NaturalClient extends Client {
+  public NaturalClient(String name) {
+    super(name);
+  }
+}

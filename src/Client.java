eb1d6f4c70dd426f3d2ec33ff1068c62c37de914
wesdell <wesdell17@gmail.com>
2024-06-06 15:43:58 -0500

    New method to add a bank account

---
+++ src/Client.java
@@ -0,0 +1,14 @@
+abstract public class Client {
+  private final String name;
+  private int registeredAccounts;
+  private Account[] accounts;
+
+  public Client(String name) {
+    this.name = name;
+    this.accounts = new Account[10];
+  }
+
+  public void addAccount(Account account) {
+    this.accounts[this.registeredAccounts++] = account;
+  }
+}

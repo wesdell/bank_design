fb1b87755b911c40a47ca9178865ba9399515bd8
wesdell <wesdell17@gmail.com>
2024-06-06 15:38:58 -0500

    Prepare code testing

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    // Dado un banco y dos tipos de clientes
+    Bank bank = new Bank("GP");
+    NaturalClient sebastian = new NaturalClient("Sebastian");
+    LegalClient edwin = new LegalClient("Edwin");
+    Account account1 = bank.createSavingAccount(sebastian);
+    Account account2 = bank.createCheckingAccount(sebastian, 150.00);
+    Account account3 = bank.createCheckingAccount(edwin, 500.00);
+
+    System.out.println(account1);
+    System.out.println(account2);
+    System.out.println(account3);
+
   }
 }

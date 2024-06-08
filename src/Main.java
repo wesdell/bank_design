f1d4a226a18ed97aa3e8b4701e12bd955844e098
wesdell <wesdell17@gmail.com>
2024-06-07 19:52:26 -0500

    Implement tests

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,16 @@ public class Main {
     Bank bank = new Bank("GP");
     NaturalClient sebastian = new NaturalClient("Sebastian");
     LegalClient edwin = new LegalClient("Edwin");
-    Account account1 = bank.createSavingAccount(sebastian);
-    Account account2 = bank.createCheckingAccount(sebastian, 150.00);
-    Account account3 = bank.createCheckingAccount(edwin, 500.00);
+    SavingAccount account1 = bank.createSavingAccount(sebastian);
+    CheckingAccount account2 = bank.createCheckingAccount(sebastian, 150.00);
+    CheckingAccount account3 = bank.createCheckingAccount(edwin, 500.00);
 
+    System.out.println(account1);
+    account1.deposit(3);
+    System.out.println(account1);
+    account1.takeOut(20);
     System.out.println(account1);
     System.out.println(account2);
     System.out.println(account3);
-
   }
 }
